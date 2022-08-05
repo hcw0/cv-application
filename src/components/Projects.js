a3c23f95dd9edf96d4f9c9e5283f464b1a7440b8
h <hchenwu02@gmail.com>
2022-08-05 00:37:12 -0400

    Add main information for project card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,37 @@ import ProjectsCSS from "./Projects.module.css";
 class Projects extends Component {
     constructor(props){
         super(props);
+
+        let defaultProject1 = {
+            title: "Stock Trading Simulator",
+            titleInputWidth: 20, 
+            moreInformation: "Python, Flask, React, PostgreSQL",
+            moreInformationInputWidth: 27,
+            date: "Jan. 2017 - May 2017",
+            dateInputWidth: 18,
+            description: [],
+        }
+
+        this.state = {
+            projects: [defaultProject1],
+        }
+    }
+
+    handleInputChange = (event, index) => {
+        let newProjects = [...this.state.projects];
+        let varName = event.target.name;
+        newProjects[index][varName] = event.target.value;
+        this.setState({
+            projects: newProjects,
+        });
+    }
+
+    changeInputWidth = (event, index) => {
+        let newProjects = [...this.state.projects];
+        newProjects[index][event.target.name + "InputWidth"] = event.target.value.length;
+        this.setState({
+            projects: newProjects,
+        });
     }
 
     render(){
@@ -12,6 +43,30 @@ class Projects extends Component {
                 <div className={ProjectsCSS.titleContainer}>
                     <p className={ProjectsCSS.title}>PROJECTS</p>
                 </div>
+
+                {this.state.projects.map((project, projectIndex) => {
+                    return (
+                        <div key={projectIndex} className={ProjectsCSS.projectsContainer}>
+                            <div className={ProjectsCSS.topContainer}>
+                                <div className={ProjectsCSS.leftContainer}>
+                                    <input style={{ width: project.titleInputWidth + "ch", borderRight: project.moreInformation != "" ? "1px solid black" : ""}} 
+                                        type="text" name="title" 
+                                        value={project.title} className={ProjectsCSS.projectTitle}
+                                        onChange={event => { this.handleInputChange(event, projectIndex); this.changeInputWidth(event, projectIndex) }}
+                                        onBlur={event => { this.setNameInputIfEmpty(event, projectIndex) }} />
+                                    <input style={{ width: project.moreInformationInputWidth + "ch" }} type="text" name="moreInformation" 
+                                        value={project.moreInformation} className={ProjectsCSS.moreInformation}
+                                        onChange={event => { this.handleInputChange(event, projectIndex); this.changeInputWidth(event, projectIndex) }}
+                                        onBlur={event => { this.setNameInputIfEmpty(event, projectIndex) }} />
+                                </div>
+                                <input style={{ width: project.dateInputWidth + "ch" }} type="text" name="date" 
+                                        value={project.date} className={ProjectsCSS.date}
+                                        onChange={event => { this.handleInputChange(event, projectIndex); this.changeInputWidth(event, projectIndex) }}
+                                        onBlur={event => { this.setNameInputIfEmpty(event, projectIndex) }} />
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         )
     }
