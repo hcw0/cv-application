ec0e3d62bb45ac3e119915e458a2303bd41163a4
h <hchenwu02@gmail.com>
2022-08-07 01:51:14 -0400

    Add reset and print button and print functionality

---
+++ src/components/Buttons.js
@@ -0,0 +1,23 @@
+import ButtonsCSS from "./Buttons.module.css"
+import React, {Component} from "react";
+
+class Buttons extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    printFunction = () => {
+        window.print()
+    }
+
+    render(){
+        return (
+            <div className={ButtonsCSS.buttons}>
+                <button className={ButtonsCSS.resetButton}>Reset</button>
+                <button onClick={this.printFunction} className={ButtonsCSS.printButton}>Print</button>
+            </div>
+        )
+    }
+}
+
+export default Buttons;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Buttons from './components/Buttons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
   <React.StrictMode>
     <App />
+    <Buttons/>
   </React.StrictMode>
 );
 
